0cf74295c370a7ed6b5097304c1809510d798cb9
Michał Jóźwiak <michajozwiak@gmail.com>
2023-10-16 18:08:39 +0200

    Interface for Borrower

---
+++ LibraryAPI/Interfaces/IBorrowerRepository.cs
@@ -0,0 +1,13 @@
+﻿using LibraryAPI.Models;
+
+namespace LibraryAPI.Interfaces
+{
+    public interface IBorrowerRepository
+    {
+        Task<IEnumerable<Borrower>> GetBorrowers();
+        Task<Borrower> GetBorrower(int id);
+        Task<Borrower> AddBorrower(Borrower borrower);
+        Task<Borrower> UpdateBorrower(Borrower borrower);
+        Task<Borrower> DeleteBorrower(int id);
+    }
+}
---
+++ LibraryAPI/Repository/BorrowerRepository.cs
@@ -0,0 +1,41 @@
+﻿using LibraryAPI.Data;
+using LibraryAPI.Interfaces;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Repository
+{
+    public class BorrowerRepository : IBorrowerRepository
+    {
+        private readonly AppDbContext _context;
+        public BorrowerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Borrower> AddBorrower(Borrower borrower)
+        {
+            await _context.Borrowers.AddAsync(borrower);
+            return borrower;
+        }
+
+        public Task<Borrower> DeleteBorrower(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Borrower> GetBorrower(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Borrower>> GetBorrowers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Borrower> UpdateBorrower(Borrower borrower)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
